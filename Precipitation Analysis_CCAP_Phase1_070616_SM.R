6e077df40f236694051bbf6165a5d2328a36713f
bastim2410 <seb.malter@gmail.com>
2016-07-06 09:37:51 -0400

    Add files via upload

---
+++ Precipitation Analysis_CCAP_Phase1_070616_SM.R
@@ -0,0 +1,74 @@
+##########################################################################################################################
+# Philadelphia Water - Office of Watersheds 
+# Program: Climate Change Adaption Program (CCAP) 
+# Project: Precipitation Analysis 																						
+#																														
+# PHASE 1: Comparing historical and future global climate model (GCM) precipitation data for Philadelphia  																																			
+#	
+# Purpose:        Code for statistical parameters of future, historic modeled precipitation data and 
+#                 historic measured data
+# 
+# Created:        7/6/2016 																																		
+# Created by:     Sebastian Malter 
+# Last Modified:  																																	
+#						
+# Related files:  o Project proposel:
+#                 \\pwdhqr\OOWS\Climate Change Adaptation\CCAP Risk Assessment\H&H\Precipitation Analysis\Precipitation Analysis_CCAP_June2016_DRAFT_updated
+#                 o Calculation protocol:
+#                 \\pwdhqr\OOWS\Climate Change Adaptation\CCAP Risk Assessment\H&H\Precipitation Analysis\Precipitation Analysis_Calculation protocol
+#                 o Archive/ file history
+#                   https://github.com/bastim2410/CCAP-Precipitation-Analysis
+#
+#######################################################################################################################
+# 
+#------ QA/ QC ------
+#
+# Checked:        
+# Status: 																															
+#																																				
+# Record of Checking																																
+#																																				
+# Is a record of this check kept elsewhere (Y/N)?																																
+# If yes, please note the location:																															
+# If no, please fill out the information below.																															
+#																																				
+# What was checked? 
+#   			
+#
+# Questions/Clarification Needed:																																
+#																																				
+# Items that need to be addressed: 
+#    
+#
+# Related: (Please note other closely related files.)																																																																						
+#																																				
+# Notes: 
+#      
+########################################################################
+
+
+# Set working directory ----------------------------------
+
+
+# Importing and editing data -----------------------------
+
+
+
+
+# Creating statistical parameters ------------------------
+
+
+
+
+# Creating CDF's -----------------------------------------
+
+
+
+
+
+
+
+
+
+
+
